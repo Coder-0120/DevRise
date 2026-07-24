19168c82c17dc10bc2ecf03eb950608ebe25e8d3
Anshul Verma <av4862187@gmail.com>
2026-07-24 23:50:00 +0530

    models updated

---
+++ Task3/server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const app = express();
+// const authRoutes = require("./routes/authRoute");
+// const contactRoutes = require("./routes/contactRoute");
+dotenv.config();
+connectDB();
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/contact", contactRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
